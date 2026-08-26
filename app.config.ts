cf11dbd1af0a90e9cfc92affd6304aa05a46f205
damirhodo19 <damirhodo@gmail.com>
2026-08-26 11:59:46 +0200

    chore: configure iOS encryption declaration

--- app.config.ts
+++ app.config.ts
@@ -20,6 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     buildNumber: '1',
     infoPlist: {
       CFBundleDisplayName: 'Handball IQ',
+      ITSAppUsesNonExemptEncryption: false,
     },
   },
   android: {
